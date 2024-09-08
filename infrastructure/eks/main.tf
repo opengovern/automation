94fc7056e346ebdd7d13a655252d7c9e4ee003f4
Anil Chandra <120965339+acx1729@users.noreply.github.com>
2024-09-08 16:33:13 -0400

    Rolling Back

--- infrastructure/eks/main.tf
+++ infrastructure/eks/main.tf
@@ -89,87 +89,7 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-    /*
-    multi-volume = {
-      instance_types = ["c5.large"]
 
-      min_size     = 1
-      max_size     = 5
-      desired_size = 3
-   
-      block_device_mappings = {
-        # Root volume
-        xvda = {
-          device_name = "/dev/xvda"
-          ebs = {
-            volume_type           = "gp3"
-            encrypted             = true
-            kms_key_id            = module.ebs_kms_key.key_arn
-            delete_on_termination = true
-          }
-        }
-        xvdb = {
-          # This will be used for containerd's data directory
-          device_name = local.second_volume_name
-          ebs = {
-            volume_size           = 24
-            volume_type           = "gp3"
-            iops                  = 3000
-            encrypted             = true
-            kms_key_id            = module.ebs_kms_key.key_arn
-            delete_on_termination = true
-          }
-        }
-      }
-     
-
-      # This user data mounts the containerd directories to the second EBS volume which
-      # is dedicated to just containerd. You can read more about the practice and why
-      # here https://aws.github.io/aws-eks-best-practices/scalability/docs/data-plane/#use-multiple-ebs-volumes-for-containers
-      # and https://github.com/containerd/containerd/blob/main/docs/ops.md#base-configuration
-      cloudinit_pre_nodeadm = [
-        {
-          content_type = "text/x-shellscript"
-          content      = <<-EOT
-            # Wait for second volume to attach before trying to mount paths
-            TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-            EC2_INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/instance-id)
-            DATA_STATE="unknown"
-            until [ "$${DATA_STATE}" == "attached" ]; do
-              DATA_STATE=$(aws ec2 describe-volumes \
-                --region ${local.region} \
-                --filters \
-                    Name=attachment.instance-id,Values=$${EC2_INSTANCE_ID} \
-                    Name=attachment.device,Values=${local.second_volume_name} \
-                --query Volumes[].Attachments[].State \
-                --output text)
-              sleep 5
-            done
-
-            # Get the volume ID
-            VOLUME_ID=$(aws ec2 describe-volumes \
-              --region ${local.region} \
-              --filters \
-                  Name=attachment.instance-id,Values=$${EC2_INSTANCE_ID} \
-                  Name=attachment.device,Values=${local.second_volume_name} \
-              --query Volumes[].Attachments[].VolumeId \
-              --output text | sed 's/-//')
-
-            # Mount the containerd directories to the 2nd volume
-            SECOND_VOL=$(lsblk -o NAME,SERIAL -d |awk -v id="$${VOLUME_ID}" '$2 ~ id {print $1}')
-            systemctl stop containerd
-            mkfs -t ext4 /dev/$${SECOND_VOL}
-            rm -rf /var/lib/containerd/*
-            rm -rf /run/containerd/*
-
-            mount /dev/$${SECOND_VOL} /var/lib/containerd/
-            mount /dev/$${SECOND_VOL} /run/containerd/
-            systemctl start containerd
-          EOT
-        }
-      ]
-    }
-    */
     instance-store = {
       instance_types = ["m6in.xlarge"]
 
@@ -236,40 +156,17 @@ module "eks_blueprints_addons" {
     kube-proxy = {}
   }
 
-  enable_velero = false
-  # An S3 Bucket ARN is required. This can be declared with or without a prefix
-  #velero = {
-  #  s3_backup_location = local.velero_s3_backup_location
-  #}
+
   enable_aws_efs_csi_driver = false
+  
+  enable_aws_load_balancer_controller = true
+  aws_load_balancer_controller = {
+    chart_version = "1.6.0" # min version required to use SG for NLB feature
+  }
 
   tags = local.tags
 }
 
-################################################################################
-# Storage Classes
-################################################################################
-/*
-resource "kubernetes_annotations" "gp2" {
-  api_version = "storage.k8s.io/v1"
-  kind        = "StorageClass"
-  # This is true because the resources was already created by the ebs-csi-driver addon
-  force = "true"
-
-  metadata {
-    name = "gp2"
-  }
-
-  annotations = {
-    # Modify annotations to remove gp2 as default storage class still retain the class
-    "storageclass.kubernetes.io/is-default-class" = "false"
-  }
-
-  depends_on = [
-    module.eks_blueprints_addons
-  ]
-}
-*/
 
 resource "kubernetes_storage_class_v1" "gp3" {
   metadata {
@@ -287,7 +184,7 @@ resource "kubernetes_storage_class_v1" "gp3" {
   volume_binding_mode    = "WaitForFirstConsumer"
 
   parameters = {
-    encrypted = true
+    encrypted = false
     fsType    = "ext4"
     type      = "gp3"
   }
@@ -297,32 +194,6 @@ resource "kubernetes_storage_class_v1" "gp3" {
   ]
 }
 
-/*
-resource "kubernetes_storage_class_v1" "efs" {
-  metadata {
-    name = "efs"
-    annotations = {
-      # Annotation to set gp3 as default storage class
-      "storageclass.kubernetes.io/is-default-class" = "false"
-    }
-  }
-
-  storage_provisioner = "efs.csi.aws.com"
-  parameters = {
-    provisioningMode = "efs-ap" # Dynamic provisioning
-    fileSystemId     = module.efs.id
-    directoryPerms   = "700"
-  }
-
-  mount_options = [
-    "iam"
-  ]
-
-  depends_on = [
-    module.eks_blueprints_addons
-  ]
-}
-*/
 ################################################################################
 # Supporting Resources
 ################################################################################
@@ -353,71 +224,7 @@ module "vpc" {
   tags = local.tags
 }
 
-/*
-module "velero_backup_s3_bucket" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "~> 3.0"
 
-  bucket_prefix = "${local.name}-"
-
-  # Allow deletion of non-empty bucket
-  # NOTE: This is enabled for example usage only, you should not enable this for production workloads
-  force_destroy = true
-
-  attach_deny_insecure_transport_policy = true
-  attach_require_latest_tls_policy      = true
-
-  acl = "private"
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-
-  control_object_ownership = true
-  object_ownership         = "BucketOwnerPreferred"
-
-  versioning = {
-    status     = true
-    mfa_delete = false
-  }
-
-  server_side_encryption_configuration = {
-    rule = {
-      apply_server_side_encryption_by_default = {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  tags = local.tags
-}
-*/
-/*
-module "efs" {
-  source  = "terraform-aws-modules/efs/aws"
-  version = "~> 1.1"
-
-  creation_token = local.name
-  name           = local.name
-
-  # Mount targets / security group
-  mount_targets = {
-    for k, v in zipmap(local.azs, module.vpc.private_subnets) : k => { subnet_id = v }
-  }
-  security_group_description = "${local.name} EFS security group"
-  security_group_vpc_id      = module.vpc.vpc_id
-  security_group_rules = {
-    vpc = {
-      # relying on the defaults provided for EFS/NFS (2049/TCP + ingress)
-      description = "NFS ingress from VPC private subnets"
-      cidr_blocks = module.vpc.private_subnets_cidr_blocks
-    }
-  }
-
-  tags = local.tags
-}
-*/
 module "ebs_kms_key" {
   source  = "terraform-aws-modules/kms/aws"
   version = "~> 1.5"
@@ -434,7 +241,7 @@ module "ebs_kms_key" {
   ]
 
   # Aliases
-  aliases = ["eks/${local.name}/ebs"]
+  #aliases = ["eks/${local.name}/ebs"]
 
   tags = local.tags
 }
